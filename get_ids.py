5274176fc0751c7f1e3c8c9bc4e226905139a4fb
Carlos A. Casado Vargas <carloscasadovargas@gmail.com>
2024-11-30 13:14:01 -0500

    get ids variant replaces

--- get_ids.py
+++ get_ids.py
@@ -1,28 +1,49 @@
 import sys
 import pandas as pd
 from _normalize import _normalize
+import argparse
 
 
 def main():
-    if len(sys.argv) > 1:
-        # csv containing ids
-        ids_csv = pd.read_csv(sys.argv[1], na_filter=False)
+    parser = argparse.ArgumentParser(description="Get ids from a csv")
 
-        # csv to compare with ids
-        cleaned_csv = pd.read_csv(sys.argv[2], na_filter=False)
+    parser.add_argument("--ids_csv", type=str, help="csv containing ids", required=True)
 
-        # column of the cleaned_csv with the values you want to find their ids in ids_csv
-        column_to_search_id = sys.argv[3]
+    parser.add_argument(
+        "--cleaned_csv", type=str, help="csv to compare with ids", required=True
+    )
 
-        # column of the ids_csv with the values you want to compare
-        # with the cleaned_csv (normally 'name')
-        column_to_compare_item = sys.argv[4]
+    parser.add_argument(
+        "--column_to_search_id",
+        type=str,
+        help="column of the cleaned_csv with the values you want to find their ids in ids_csv",
+        required=True,
+    )
 
-        get_ids(ids_csv, cleaned_csv, column_to_search_id, column_to_compare_item)
-    else:
-        print(
-            "Usage: python get_ids.py ids_file.csv cleaned_file.csv column_to_search_id column_to_compare_item (normally 'name')"
-        )
+    parser.add_argument(
+        "--column_to_compare_item",
+        type=str,
+        help="column of the ids_csv with the values you want to compare with the cleaned_csv (normally 'name')",
+        required=True,
+    )
+
+    parser.add_argument(
+        "--variant_replaces",
+        type=str,
+        help="Dictionary in key:value,key:value format to replace in the column_to_search_id before comparing",
+    )
+
+    args = parser.parse_args()
+
+    print(args.variant_replaces, type(args.variant_replaces))
+
+    get_ids(
+        pd.read_csv(args.ids_csv, na_filter=False),
+        pd.read_csv(args.cleaned_csv, na_filter=False),
+        args.column_to_search_id,
+        args.column_to_compare_item,
+        args.variant_replaces,
+    )
 
 
 def get_ids(
@@ -30,7 +51,13 @@ def get_ids(
     cleaned_csv: pd.DataFrame,
     column_to_search_id: str,
     column_to_compare_item: str,
+    variant_replaces: str,
 ):
+    if variant_replaces:
+        variant_replaces = dict(
+            [tuple(x.split(":")) for x in variant_replaces.split(",")]
+        )
+
     # traverse the cleaned csv and take the value of the column_to_search_id
     for index, row in cleaned_csv.iterrows():
         item = row[column_to_search_id]
@@ -39,6 +66,13 @@ def get_ids(
         # if not name:
         #     cleaned_csv.at[index, column_to_search_id] = "None"
         #     continue
+
+        if variant_replaces:
+            if item in variant_replaces:
+                print(f"\t[!] Replacing {item} with {variant_replaces[item]}")
+                item = variant_replaces[item]
+            else:
+                print(f"\t[!] No replacements for {item}")
 
         # traverse the ids csv and compare the item with the column_to_compare_item
         # if they are the same, take the id and save it in the cleaned csv
@@ -50,11 +84,12 @@ def get_ids(
 
                 break
 
-    # cleaned_csv.to_csv("ided_" + sys.argv[2], index=False)
     cleaned_csv.to_csv(
-        f"ided_{
-            sys.argv[3]
-        }_{sys.argv[2].split('/')[-1].split('.')[0]}.csv", index=False
+        # name of the column to search id + name of the cleaned csv file
+        f"ided_{column_to_search_id}_{
+            sys.argv[4].split('/')[-1].split('.')[0]
+        }.csv",
+        index=False,
     )
 
 
