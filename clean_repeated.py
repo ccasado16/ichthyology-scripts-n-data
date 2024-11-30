e95d147e94c1a56d4baf6a6e43c3d2b7befb6e44
Carlos A. Casado Vargas <carloscasadovargas@gmail.com>
2024-11-29 23:21:21 -0500

    new cleaned_nombre_cientifico_autor_raw

--- clean_repeated.py
+++ clean_repeated.py
@@ -7,13 +7,6 @@ import argparse
 def main():
     parser = argparse.ArgumentParser()
 
-    # if len(sys.argv) > 1:
-    #     csv = pd.read_csv(sys.argv[1], na_filter=False)
-    #     column_name = sys.argv[2]
-    #     clean(csv, column_name)
-    # else:
-    #     print("Usage: python clean_repeated.py file-raw.csv column_name_to_sort")
-    #     return
     parser.add_argument(
         "--csv",
         type=str,
@@ -42,20 +35,14 @@ def main():
     )
 
 
-def clean(
-    csv: pd.DataFrame, column_name: str, characs_to_remove: list = None
-):
+def clean(csv: pd.DataFrame, column_name: str, characs_to_remove: list = None):
     csv.dropna(inplace=True)
     raw_items_array = csv.values
 
     # raw items is a list of lists of strings, turn all the strings into lowercase and if the string is empty, replace it with "None"
     raw_items_array = [
         [
-            (
-                _normalize(str(item), characs_to_remove)
-                if item != ""
-                else "None"
-            )
+            (_normalize(str(item), characs_to_remove) if item != "" else "None")
             for item in row
         ]
         for row in raw_items_array
