cdc7acb8971c4d623debf6c19d81eaa5ad39fbf9
Carlos A. Casado Vargas <carloscasadovargas@gmail.com>
2024-11-29 22:13:59 -0500

    argparse and characs_to_remove in clean_repeated.py

--- clean_repeated.py
+++ clean_repeated.py
@@ -1,25 +1,63 @@
 import sys
 import pandas as pd
 from _normalize import _normalize
+import argparse
 
 
 def main():
-    if len(sys.argv) > 1:
-        csv = pd.read_csv(sys.argv[1], na_filter=False)
-        column_name = sys.argv[2]
-        clean(csv, column_name)
-    else:
-        print("Usage: python clean_repeated.py file-raw.csv column_name_to_sort")
-        return
+    parser = argparse.ArgumentParser()
+
+    # if len(sys.argv) > 1:
+    #     csv = pd.read_csv(sys.argv[1], na_filter=False)
+    #     column_name = sys.argv[2]
+    #     clean(csv, column_name)
+    # else:
+    #     print("Usage: python clean_repeated.py file-raw.csv column_name_to_sort")
+    #     return
+    parser.add_argument(
+        "--csv",
+        type=str,
+        required=True,
+        help="The csv file to clean",
+    )
+    parser.add_argument(
+        "--column_name",
+        type=str,
+        required=True,
+        help="The column name to sort",
+    )
+
+    parser.add_argument(
+        "--characs_to_remove",
+        nargs="+",
+        help="List of characters to remove in the rows of the columns",
+    )
+
+    args = parser.parse_args()
+
+    clean(
+        pd.read_csv(args.csv, na_filter=False),
+        args.column_name,
+        args.characs_to_remove,
+    )
 
 
-def clean(csv: pd.DataFrame, column_name: str):
+def clean(
+    csv: pd.DataFrame, column_name: str, characs_to_remove: list = None
+):
     csv.dropna(inplace=True)
     raw_items_array = csv.values
 
     # raw items is a list of lists of strings, turn all the strings into lowercase and if the string is empty, replace it with "None"
     raw_items_array = [
-        [_normalize(str(item)) if item != "" else "None" for item in row]
+        [
+            (
+                _normalize(str(item), characs_to_remove)
+                if item != ""
+                else "None"
+            )
+            for item in row
+        ]
         for row in raw_items_array
     ]
 
@@ -43,7 +81,7 @@ def clean(csv: pd.DataFrame, column_name: str):
     cleaned_csv.to_csv(
         f"cleaned_{
         # remove the parent directory and the extension of the file to create the new file name
-        sys.argv[1].split("/")[-1].split(".")[0]}.csv",
+        sys.argv[2].split("/")[-1].split(".")[0]}.csv",
         index=False,
     )
 
