b32546ede6b4ad4244de207b841201b2107abd97
Carlos A. Casado Vargas <carloscasadovargas@gmail.com>
2024-12-01 15:17:05 -0500

    get usages ids

--- _split.py
+++ _split.py
@@ -4,6 +4,7 @@ def _split(row):
         .replace("&", " , ")
         .replace("/", " , ")
         .replace("-", " , ")
+        # .replace(",", " , ") # for Usage or Usos table
         .split(" , ")
     )
     splitted = [x.strip() for x in splitted]
